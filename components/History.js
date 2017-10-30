b14a292ae9cbe24708e12bdd284b5e2842026df5
zhoujie <zhoujiejay@didichuxing.com>
2017-10-30 16:38:29 +0800

    add item style

--- components/History.js
+++ components/History.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Platform, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions/index'
 import { fetchCalenderResultes } from '../utils/api'
 import { timeToString, getLoggedDisplayText } from '../utils/helpers'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
 
 class History extends Component {
 
@@ -22,7 +23,7 @@ class History extends Component {
   }
 
   renderItem = ({today, ...metrics}, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today
         ? <Text>{JSON.stringify(today)}</Text>
         : <Text>{JSON.stringify(metrics)}</Text>
@@ -49,6 +50,26 @@ class History extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    }
+
+  }
+})
 
 function mapStateToProps(entries) {
   return {
